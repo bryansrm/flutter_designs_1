ef13a30f95a8441e0c0aa4eda626b6e9fe3261da
Bryan Rodriguez <bsteven1993@gmail.com>
2020-02-03 17:48:55 -0500

    cambios pantalla basica

--- lib/src/pages/basico.dart
+++ lib/src/pages/basico.dart
@@ -35,9 +35,62 @@ class BasicoPage extends StatelessWidget {
                 Text('41', style: TextStyle(fontSize: 20.0),)
               ],
             ),
-          )
+          ),
+          _createActions(),
+          _createText(),
+
         ],
       )
     );
   }
+
+  Widget _createActions(){
+    
+    List<Widget> listActions = [];
+    List<dynamic> actions = [
+      {
+        'text' : 'CALL',
+        'icon' : Icons.call
+      },
+      {
+        'text' : 'ROUTE',
+        'icon' : Icons.near_me
+      },
+       {
+        'text' : 'SHARE',
+        'icon' : Icons.share
+      }
+    ];
+
+    for (var action in actions) {
+      
+      listActions.add(
+        Column(
+          children: <Widget>[
+            Icon( action['icon'] , color: Colors.blue,),
+            SizedBox(height: 10.0,),
+            Text( action['text'] , style: TextStyle(color: Colors.blue),)
+          ],
+        ),
+      );
+
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: listActions,
+    );
+  
+  }
+
+  Widget _createText(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      child: Text(
+        'Do pariatur ullamco et ipsum anim incididunt dolore dolore. Est amet magna consequat irure reprehenderit magna consectetur aute voluptate officia elit. Irure consectetur tempor sunt labore culpa cillum do ullamco. Laborum aliquip consectetur Lorem non laboris. Ad consectetur irure voluptate nisi elit tempor ullamco non. Sit esse voluptate do duis non ut aliquip in irure consectetur.',
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
 }
