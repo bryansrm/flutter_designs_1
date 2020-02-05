750eb959844ec2f1439542b9edf1a51bdebabaa3
Bryan Rodriguez <bsteven1993@gmail.com>
2020-02-04 23:06:35 -0500

    pagina botones con gradient

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Basico Page',
-      initialRoute: 'scroll',
+      initialRoute: 'bootones',
       routes: getAplicationsRoutes(),
     );
   }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,11 +1,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_disenios/src/pages/basico.dart';
+import 'package:flutter_disenios/src/pages/botones_page.dart';
 import 'package:flutter_disenios/src/pages/scroll_page.dart';
 
 Map<String, WidgetBuilder> getAplicationsRoutes(){
   return <String, WidgetBuilder>{
     'basico' : (context) => BasicoPage(),
     'scroll' : (context) => ScrollPage(),
+    'botones' : (context) => BotonesPage(),
   };
 }
